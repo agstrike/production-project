961f9f2975866bcf0912d922ddd532edb0f2acad
Simon Hanna <simhnna@gmail.com>
2018-04-30 21:53:55 +0200

    Add settings for other database types

--- settings.py
+++ settings.py
@@ -86,6 +86,18 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #    'NAME': 'silverstrikedb',
+    #    'USER': 'silverstrike',
+    #    'PASSWORD': 'password',
+    #}
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'silverstrikedb',
+    #    'USER': 'silverstrike',
+    #    'PASSWORD': 'password',
+    #}
 }
 
 
